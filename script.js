8e64c9e06ff94d0ca08872a450c13f4e971b2a15
pikimaru <pikajunglist@gmail.com>
2022-06-14 20:54:09 +0200

    The logic is done, now its time for css

--- script.js
+++ script.js
@@ -4,19 +4,7 @@ let bookList = document.getElementById('book--list')
 
 let books = [];
 
-let book1 = {
-    id:"202020",
-    author:'Adam',
-    title:'potop',
-    pages:10,
-}
-let book2 = {
-    id:"202021",
-    author:'Karol',
-    title:'wojtyla',
-    pages:2137
-}
-books.push(book1,book2)
+
 
 
 let addBook = function(ev){
@@ -26,6 +14,7 @@ let addBook = function(ev){
         author: document.getElementById('author').value,
         title: document.getElementById("title").value,
         pages:document.getElementById('pages').value,
+        read:document.getElementById('checkboxIn').checked,
     }
     books.push(book)
     document.forms[0].reset();
@@ -36,6 +25,33 @@ btnadd.addEventListener('click',addBook)
 function rmv(array, element){
     let index = array.indexOf(element);
     array.splice(index,1)
+    console.log(index)
+}
+
+function clicked(clicked_id){
+
+    return clicked_id
+}
+
+function toggle(array,element){
+
+    function searchid(book){
+        return book.id == element
+    }
+    let findindex = array.findIndex(searchid)
+    
+    let readornot = (array[findindex].read)
+        if ((array[findindex].read)== 'yes'){
+            let tickCheck = document.getElementById('checkbox').checked = 'false'
+            array[findindex].read = 'no'
+        }else if((array[findindex].read)=='no'){
+            let tickCheck = document.getElementById('checkbox').checked = 'true'
+            array[findindex].read = 'yes'
+        }
+
+
+
+
 }
 
 let refreshBookStore = function(){
@@ -44,18 +60,44 @@ let refreshBookStore = function(){
 
         let NewDiv = document.createElement("div");
         let closebut = document.createElement("button")
+        let togglebut = document.createElement("div")
+       
+        togglebut.innerHTML = `<label id="switch" > 
+                                    <input type="checkbox">
+                                    <span class="slider">Read</span>
+                                </label>`
+
+        togglebut.classList.add(books[i].id)
+
+        togglebut.onclick = function(){
+            let tbval = this.classList.value
+            toggle(books,tbval)
+            
+
+        }
+
         closebut.innerHTML = "delete"
+        
+        
+        
+        bookList.appendChild(togglebut)
+
         bookList.appendChild(closebut)
         bookList.appendChild(NewDiv)
+
         NewDiv.textContent = JSON.stringify(books[i])
         NewDiv.dataset.name = books[i].id
         closebut.dataset.name = books[i].id
         closebut.classList.add(books[i].id)
+
+
         closebut.onclick = function(){
             rmv(books,books[i].id)
             refreshBookStore()
         }
+
         }
+
         }
 
 
